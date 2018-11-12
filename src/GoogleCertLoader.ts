228ed53f2a5edec9740b94bc9786872adaeeaba8
Marko Harjula <marko.harjula@gmail.com>
2018-11-12 13:18:31 +0200

    Update GoogleCertLoader.ts

--- src/GoogleCertLoader.ts
+++ src/GoogleCertLoader.ts
@@ -2,6 +2,7 @@ import CertLoader from './CertLoader';
 import fetch from 'cross-fetch';
 
 // TODO: build v3 loader
+// check https://github.com/tracker1/node-rsa-pem-from-mod-exp for Google public mod/exp 
 export class GoogleCertLoader extends CertLoader {
 	private static instance: CertLoader = new GoogleCertLoader();
 	public get() {
